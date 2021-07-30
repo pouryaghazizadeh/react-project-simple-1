d23f5d0ac7871dd5d902f2eede8b531d1ddaa82e
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-07-30 16:57:00 +0200

    bug1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,75 @@
-import Header from "./component/Header";
+import SearchBox from "./component/SearchBox";
 import styled from "styled-components";
-const Div = styled.div`
-  padding: 0px;
-  margin: 0px;
-`;
+import { Switch, Route,NavLink} from "react-router-dom";
+import Home from "./component/Home";
+import Aboute from "./component/Aboute";
 function App() {
   return (
+   
     <Div>
-      <Header />
+    <H1>TvMaze</H1>
+     <SearchBox/>
+     <Ulstyle>
+        <Li>
+          <NavLink to="/">Home</NavLink>
+        </Li>
+        <Li>
+          <NavLink to="/About">about</NavLink>
+        </Li>
+        <Li>
+          <NavLink to="Login">Login</NavLink>
+        </Li>
+      </Ulstyle>
+      
+
+     
+      <Switch>
+      <Route path="/Home" exact><Home/></Route>
+      <Route path ="/Aboute" ><Aboute/></Route>
+      <Route></Route>
+      <Route></Route>
+      </Switch>
     </Div>
+
+   
+ 
+
+   
   );
 }
+
+// style for ul header
+const Ulstyle = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  justify-content: space-around;
+  align-items: center;
+  width: 250px;
+`;
+const Li = styled.li`
+  text-decoration: none;
+`;
+
+// style for header website
+const Div = styled.div`
+  height: 100px;
+  width: 100vw;
+  padding: 0px;
+  margin: 0px;
+  display: flex;
+  /* justify-content: flex-end; */
+  background-color: blue;
+  align-items: center;
+  align-content: space-between;
+  flex-wrap: wrap;
+`;
+const H1 = styled.h1`
+  padding: 0px;
+  margin: 0px;
+  display: flex;
+  align-self: flex-start;
+  
+`;
 
 export default App;
---
+++ src/component/Aboute.jsx
@@ -0,0 +1,14 @@
+const Aboute =()=>{
+    return(
+        <>
+      
+      <p>fghfdhfhfdhfdhdfhfhfdhf</p>
+       <h1> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi dicta excepturi tempore ea amet vitae, magni reprehenderit? Minima, blanditiis. Quas qui voluptatibus quia mollitia cumque? Commodi recusandae, veritatis adipisci libero pariatur voluptatum, necessitatibus eum, dolore cumque numquam inventore alias. Eaque.</h1>
+        </>
+
+
+
+    )
+}
+
+export default Aboute
