3c4350f238c05bc74f071fe3b3c00ddb3affcb8e
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-08-04 16:13:46 +0200

    make searchBox give  style

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,20 +1,34 @@
 // import SearchBox from "../SearchBox";
+// import styled from "styled-components";
 import { NavLink } from "react-router-dom";
-import styled from "styled-components";
 import { FaBars } from "react-icons/fa";
-
+import "../navbar/navbar.css";
 //
 //
 const Navbar = () => {
   return (
     <div>
-      <nav>
-        <H1>TvMaze</H1>
+      <nav className="mother-nav">
+        <h1>TvMaze</h1>
+        <input type="search" placeholder="Search" className="search-box" onChange={(e) => {}} />
         <FaBars className="Bars" onClick={() => {}} />
-        <div>
-          <NavLink exact to="/">Home</NavLink>
-          <NavLink exact to="/About">About</NavLink>
-          <NavLink exact to="/Login">Login</NavLink>
+
+        <div className="nave-menu">
+          <NavLink
+            exact
+            to="/"
+            className="NavLink"
+            activeStyle={{ color:"#15cdfc" }}
+          >
+            Home
+          </NavLink>
+          <NavLink exact to="/About" className="NavLink"
+          activeStyle={{ color:"#15cdfc" }}>
+            About
+          </NavLink>
+          <NavLink exact to="/Login" className="Log-in">
+            Login
+          </NavLink>
         </div>
       </nav>
     </div>
@@ -51,44 +65,44 @@ const Navbar = () => {
   );
 };
 // style div that have h1 and searchbox
-const HSstyle = { width: "50vw", display: "flex", alignItems: "baseline" };
+// const HSstyle = { width: "50vw", display: "flex", alignItems: "baseline" };
 
 // Nav link style
-const NnLink = {
-  textDecoration: "none",
-  margin: "17px",
-};
+// const NnLink = {
+//   textDecoration: "none",
+//   margin: "17px",
+// };
 
-// style for ul header
-const Ulstyle = styled.ul`
-  list-style: none;
-  display: flex;
-  justify-content: center;
-  justify-content: space-around;
-  align-items: center;
-  width: 30vw;
-`;
-const Li = styled.li`
-  text-decoration: none;
-`;
+// // style for ul header
+// const Ulstyle = styled.ul`
+//   list-style: none;
+//   display: flex;
+//   justify-content: center;
+//   justify-content: space-around;
+//   align-items: center;
+//   width: 30vw;
+// `;
+// const Li = styled.li`
+//   text-decoration: none;
+// `;
 
-// style for header website
-const Div = styled.div`
-  height: 100px;
-  width: 100vw;
-  padding: 0px;
-  margin: 0px;
-  display: flex;
-  justify-content: space-between;
-  background-color: blue;
-  align-items: center;
-  align-content: space-between;
-  flex-wrap: wrap;
-`;
-const H1 = styled.h1`
-  padding: 10px;
-  margin: 0px;
-  display: flex;
-  align-self: flex-start;
-`;
+// // style for header website
+// const Div = styled.div`
+//   height: 100px;
+//   width: 100vw;
+//   padding: 0px;
+//   margin: 0px;
+//   display: flex;
+//   justify-content: space-between;
+//   background-color: blue;
+//   align-items: center;
+//   align-content: space-between;
+//   flex-wrap: wrap;
+// `;
+// const H1 = styled.h1`
+//   padding: 10px;
+//   margin: 0px;
+//   display: flex;
+//   align-self: flex-start;
+// `;
 export default Navbar;
