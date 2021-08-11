c90ab7a4cf99a881c352f1e4eb43a121937af836
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-08-11 23:03:59 +0200

    maked button mood

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -4,23 +4,24 @@ import { NavLink } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import "../navbar/navbar.css";
 import { useState} from "react";
+import Button from "../Button DarkMood/Button";
 
 
 //
 //
 
 
-const Navbar = ({getData}) => {
+const Navbar = ({getData,getMood}) => {
   const [Id, setId] = useState(false); //this is for berger and mobile screen
   const [data, setData] = useState("");
   getData(data)
-  
   return (
     <div className="div-compo" id={Id ? "div-compo2" : ""}>
       <nav className="mother-nav" id={Id ? "mother-nav2" : ""}>
         <h1 className="name-web" id={Id ? "name-web2" : ""}>
           TvMaze
         </h1>
+        <Button getMood = {getMood}/>
         <input
           type="search"
           value={data}
