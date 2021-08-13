7fee3fab0908f247fd591942131117f4e37310a4
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-08-14 00:48:07 +0200

    add img to home

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,9 +1,11 @@
-import Card from "./card/Card";
+import Card from "../card/Card";
+import "./home.css"
 const Home = ({ update,mood,DarkMood }) => {
   return (
     <div
     style = {mood ?DarkMood.darkMood:DarkMood.lightMood}
     >
+    <img className = "img-gameOfThrones" alt = "game of thrones" src ="https://images7.alphacoders.com/475/thumbbig-475264.webp" />
       <Card update={update}  mood={mood} DarkMood={DarkMood}  />
     </div>
   );
