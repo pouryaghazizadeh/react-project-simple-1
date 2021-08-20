3861d3191508cbb100c9397d08ab83c7235a3a06
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-08-20 13:56:51 +0200

    end develop

--- src/components/buttonLogin/ButtonLogIn.jsx
+++ src/components/buttonLogin/ButtonLogIn.jsx
@@ -1,19 +1,12 @@
 
 import "./buttonLogInStyle.css";
-import { toast} from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-function ButtonLogIn() {
-  const notify = () => {
-    toast("Default Notification !");
 
-    toast.success("Success Notification !", {
-      position: toast.POSITION.TOP_CENTER,
-    });
-  };
+function ButtonLogIn() {
+
 
   return (
     <div>
-      <button className="buton-login" onClick ={notify}>
+      <button className="buton-login">
         Login
       </button>
     
